817f771946db36c17cf1d1806f4ff871d56319a9
Rostyslav <rostyslav.db@gmail.com>
2020-08-27 14:45:16 +0300

    add perturbated localize and fix test assertions

--- src/localizer.rs
+++ src/localizer.rs
@@ -4,6 +4,9 @@ use super::core::{create_leaf_transform, Bintest, ComparisonNode};
 use image::GrayImage;
 use na::{Point2, Point3, Vector2};
 
+use rand::distributions::Uniform;
+use rand::{Rng, RngCore};
+
 type Tree = Vec<ComparisonNode>;
 type Predictions = Vec<Point2<f32>>;
 type Stage = Vec<(Tree, Predictions)>;
@@ -14,17 +17,22 @@ pub struct Localizer {
     dsize: usize,
     scale: f32,
     stages: Stages,
+    distrs: (Uniform<f32>, Uniform<f32>),
 }
 
 impl Localizer {
     pub fn localize(&self, image: &GrayImage, roi: &Point3<f32>) -> Point2<f32> {
         let mut roi = roi.clone();
+        self.localize_mut(image, &mut roi);
+        roi.xy()
+    }
 
+    pub fn localize_mut(&self, image: &GrayImage, point: &mut Point3<f32>) {
         for stage in self.stages.iter() {
             let mut dvec = Vector2::new(0.0, 0.0);
 
             for (codes, preds) in stage.iter() {
-                let transform = create_leaf_transform(&roi);
+                let transform = create_leaf_transform(&point);
 
                 let idx = (0..self.depth).fold(0, |idx, _| {
                     let bintest = codes[idx].bintest(image, &transform);
@@ -38,12 +46,38 @@ impl Localizer {
                 dvec.y += pred.y;
             }
 
-            roi.x += dvec.x * roi.z;
-            roi.y += dvec.y * roi.z;
+            point.x += dvec.x * point.z;
+            point.y += dvec.y * point.z;
 
-            roi.z *= self.scale;
+            point.z *= self.scale;
         }
-        roi.xy()
+    }
+
+    pub fn perturb_localize(
+        &self,
+        image: &GrayImage,
+        roi: &Point3<f32>,
+        mut rng: impl RngCore,
+        nperturbs: usize,
+    ) -> Point2<f32> {
+        let mut xs: Vec<f32> = Vec::with_capacity(nperturbs);
+        let mut ys: Vec<f32> = Vec::with_capacity(nperturbs);
+
+        // println!("\ninit: {}", roi);
+        for _ in 0..nperturbs {
+            let z = rng.sample(self.distrs.0) * roi.z;
+            let x = roi.z.mul_add(rng.sample(self.distrs.1), roi.x);
+            let y = roi.z.mul_add(rng.sample(self.distrs.1), roi.y);
+            let mut _roi = Point3::new(x, y, z);
+
+            // println!("rand: {}", _roi);
+            self.localize_mut(image, &mut _roi);
+
+            xs.push(_roi.x);
+            ys.push(_roi.y);
+        }
+
+        Point2::new(odd_median_mut(&mut xs), odd_median_mut(&mut ys))
     }
 
     pub fn from_readable(mut readable: impl Read) -> Result<Self, Error> {
@@ -101,22 +135,31 @@ impl Localizer {
             dsize: pred_size,
             scale,
             stages,
+            distrs: (Uniform::new(0.925, 0.94), Uniform::new(-0.075, 0.075)),
         })
     }
+}
+
+pub fn odd_median_mut(numbers: &mut Vec<f32>) -> f32 {
+    numbers.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    numbers[numbers.len() >> 1]
 }
 
 #[cfg(test)]
 mod tests {
     use image::DynamicImage;
-    use std::fs::{File, create_dir};
+    use std::fs::File;
     use std::io::{BufRead, BufReader};
     use std::path::Path;
+
+    use rand::SeedableRng;
+    use rand_xorshift::XorShiftRng;
 
     use super::*;
     use crate::models::download_puploc;
 
     fn load_model() -> Localizer {
-        let path = Path::new("./assets/puploc.bin");
+        let path = Path::new("./models/puploc.bin");
 
         if !path.exists() {
             download_puploc(path).expect("model download error");
@@ -133,21 +176,12 @@ mod tests {
         }
     }
 
-    fn test_localizer_prediction(
-        localizer: &Localizer,
-        image: &GrayImage,
-        test_point: &Point2<f32>,
-        epsilon: f32,
-    ) {
-        let mut init_point = test_point.xyx();
-        init_point.x += -15.0;
-        init_point.y += 15.0;
-        init_point.z = 45.0;
-
-        let pupil_point = localizer.localize(&image, &init_point);
-
-        abs_diff_eq!(test_point.x, pupil_point.x, epsilon = epsilon);
-        abs_diff_eq!(test_point.y, pupil_point.y, epsilon = epsilon);
+    fn create_init_point(point: &Point2<f32>) -> Point3<f32> {
+        let mut init_point = point.xyx();
+        init_point.x += 5.0;
+        init_point.y += 5.0;
+        init_point.z = 40.0;
+        init_point
     }
 
     fn load_test_data(path: &Path) -> (Point2<f32>, Point2<f32>) {
@@ -193,26 +227,50 @@ mod tests {
         let last_pred_test = Point2::new(0.05820565, 0.02249731);
         let first_pred = stages[0][0].1[0];
         let last_pred = stages[stages.len() - 1][trees - 1].1[dsize - 1];
-        abs_diff_eq!(first_pred_test.x, first_pred.x);
-        abs_diff_eq!(first_pred_test.y, first_pred.y);
-        abs_diff_eq!(last_pred_test.x, last_pred.x);
-        abs_diff_eq!(last_pred_test.y, last_pred.y);
+        assert_abs_diff_eq!(first_pred_test, first_pred);
+        assert_abs_diff_eq!(last_pred_test, last_pred);
     }
 
     #[test]
     fn check_pupil_localization() {
         let assets_dir = Path::new("./assets/");
 
-        if !assets_dir.exists() {
-            create_dir(&assets_dir).expect("cannot create assets dir for testing");
-        }
-
-        let image_path = assets_dir.join("test_image.jpg");
+        let image_path = assets_dir.join("Lenna_grayscale_test.jpg");
         let puploc = load_model();
         let image = load_test_image(&image_path);
-        let (left_pupil, right_pupil) = load_test_data(&image_path.with_extension("eye"));
+        let (left_pupil, right_pupil) = load_test_data(&image_path.with_extension("txt"));
 
-        test_localizer_prediction(&puploc, &image, &left_pupil, 3.0);
-        test_localizer_prediction(&puploc, &image, &right_pupil, 3.0);
+        let epsilon = 3f32;
+        let pred = puploc.localize(&image, &create_init_point(&left_pupil));
+        assert_abs_diff_eq!(left_pupil, pred, epsilon = epsilon);
+
+        let pred = puploc.localize(&image, &create_init_point(&right_pupil));
+        assert_abs_diff_eq!(right_pupil, pred, epsilon = epsilon);
+    }
+
+    #[test]
+    fn check_perturbated_pupil_localization() {
+        let assets_dir = Path::new("./assets/");
+
+        let image_path = assets_dir.join("Lenna_grayscale_test.jpg");
+        let puploc = load_model();
+        let image = load_test_image(&image_path);
+        let (left_pupil, right_pupil) = load_test_data(&image_path.with_extension("txt"));
+
+        let mut rng = XorShiftRng::seed_from_u64(42u64);
+
+        let epsilon = 1.5f32;
+        let nperturbs = 31usize;
+        let pred =
+            puploc.perturb_localize(&image, &create_init_point(&left_pupil), &mut rng, nperturbs);
+        assert_abs_diff_eq!(left_pupil, pred, epsilon = epsilon);
+
+        let pred = puploc.perturb_localize(
+            &image,
+            &create_init_point(&right_pupil),
+            &mut rng,
+            nperturbs,
+        );
+        assert_abs_diff_eq!(right_pupil, pred, epsilon = epsilon);
     }
 }
