518630f1682825336100f13892a1924d1a64788c
Rostyslav Bohomaz <rostyslav.db@gmail.com>
2020-08-31 16:11:16 +0300

    add claster detections, test and bench for it

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,15 @@ extern crate rand_xorshift;
 #[macro_use]
 extern crate approx;
 
+#[macro_use]
+extern crate derive_new;
+
 mod core;
 mod localizer;
 mod detector;
 
 pub use localizer::Localizer;
-pub use detector::Detector;
+pub use detector::{Detector, CascadeParameters, Detection};
 pub use crate::core::create_xorshift_rng;
 
 pub mod test_utils;
