5739cc854ef5dcc2dc1074b5ba7ed8557c86d91b
Rostyslav Bohomaz <rostyslav.db@gmail.com>
2023-05-05 00:31:33 +0300

    bufreader for example

--- examples/detect-faces/models.rs
+++ examples/detect-faces/models.rs
@@ -10,10 +10,10 @@ macro_rules! model_path {
 #[macro_export]
 macro_rules! load_model {
     ($model:ident, $path:expr, $name:literal) => {
-        $model::load(
-            std::fs::File::open($path).context(format!("Cannot find {} model file.", $name))?
-        )
-        .context(format!("Invalid {} model file.", $name))?
+        $model::load({
+            let file = std::fs::File::open($path).context(format!("Cannot find {} model file.", $name))?;
+            std::io::BufReader::new(file)
+        }).context(format!("Invalid {} model file.", $name))?
     }
 }
 
